5d764eefe485d1e46a0671c53c1776ad5a557cdd
sakshamraj4 <saksham.raj@arya.ag>
2024-07-04 11:29:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-import plotly.figure_factory as ff
 import json
-from datetime import datetime, timedelta
-import numpy as np
 from urllib.parse import unquote
 
 # App code
@@ -68,20 +64,28 @@ if uploaded_file is not None:
 
         query_params = st.experimental_get_query_params()
         farm_name_param = query_params.get('farm_name', [None])[0]
+        severity_param = query_params.get('severity', [None])[0]
+        
         if farm_name_param:
             farm_name_param = unquote(farm_name_param)
-            st.write(f"Decoded farm_name_param: {farm_name_param}")  # Debugging line
-
+        
+        if severity_param:
+            severity_param = unquote(severity_param)
+        
         if farm_name_param and farm_name_param in farms:
-            default_index = list(farms).index(farm_name_param)
-            st.write(f"Default index for farm_name_param: {default_index}")  # Debugging line
+            default_farm_index = list(farms).index(farm_name_param)
         else:
-            default_index = 0
-
-        selected_farm = st.sidebar.selectbox("Select Farm", farms, index=default_index)
+            default_farm_index = 0
 
         severity_levels = ['Select All'] + list(data['Severity'].dropna().unique())
-        selected_severity = st.sidebar.selectbox("Severity", severity_levels)
+        
+        if severity_param and severity_param in severity_levels:
+            default_severity_index = severity_levels.index(severity_param)
+        else:
+            default_severity_index = 0
+
+        selected_farm = st.sidebar.selectbox("Select Farm", farms, index=default_farm_index)
+        selected_severity = st.sidebar.selectbox("Severity", severity_levels, index=default_severity_index)
 
         if selected_farm:
             if selected_severity == 'Select All':
